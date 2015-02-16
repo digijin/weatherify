92fe732851f6d1583b5def161ce2a4f10a875329
James Smith <james@digijin.com>
2015-02-16 21:50:07 +1100

    basic spec

---
+++ spec/weather.spec.js
@@ -0,0 +1,23 @@
+/*jshint -W030 */
+expect = require('chai').expect;
+
+weather = require('../app/repositories/weather');
+
+alcatraz = {
+	arr: [37.8267,-122.423],
+	obj: {lat: 37.8267, lon: -122.423},
+	str: 'alcatraz'
+}
+
+describe('weather', function(){
+
+	it('should be defined', function(){
+		expect(weather).not.to.be.an('undefined');
+		expect(weather.get).not.to.be.an('undefined');
+	});
+
+	it('should fetch weather for a location using an array', function(){
+		expect(weather.get()).to.be.an('object');
+	});
+
+});
