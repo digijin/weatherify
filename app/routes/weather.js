274ec168828eb7f264637de6aa232188b5c54bd3
James Smith <james@digijin.com>
2015-02-17 01:33:25 +1100

    json trigger

--- app/routes/weather.js
+++ app/routes/weather.js
@@ -1,8 +1,15 @@
 weather = require('../repositories/weather');
+url = require('url');
 module.exports = function(req, res){
-	console.log(req.params);
+	json = typeof url.parse(req.url, true).query.json == "string";
 	weather.get(req.params.location, req.params.time).then(function(data){
-		res.send(data);
-	})
+		console.log("json", json);
+		if(json){
+
+			res.send(data);
+		}else{
+			res.send("yoloswag240")
+		}
+	});
 	
 };
