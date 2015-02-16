b72ba31028fb4365b09376c3f68083c8f6773644
James Smith <james@digijin.com>
2015-02-17 01:19:15 +1100

    optional time

--- app/server.js
+++ app/server.js
@@ -10,6 +10,6 @@ app.get('/', function(req,res){
 	res.send(index);
 });
 
-app.get('/weather/:location/:time', require('./routes/weather'));
+app.get('/weather/:location/:time?', require('./routes/weather'));
 
 app.listen(port);
